9a811b11b6604e2db6e8a9e9cc7ba3df621ae8d9
Luis Licon <luisliconna@gmail.com>
2023-01-17 15:59:20 -0300

    agregué img y algunos estilos

--- App.js
+++ App.js
@@ -1,145 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, FlatList, Modal } from 'react-native';
+import React, { useState } from "react"
+import { StyleSheet, Text, View, Button, FlatList, Image,} from "react-native"
+import Modal from "./src/components/Modal"
+import AddItem from "./src/components/AddItem"
 
 export default function App() {
+  const [textItem, setTextItem] = useState("")
+  const [list, setList] = useState([])
+  const [itemSelected, setItemSelected] = useState("")
+  const [modalVisible, setModalVisible] = useState(false)
 
-  const [textItem, setTextItem] = useState("");
-  const [list, setList] = useState([]);
-  const [itemSelected, setItemSelected] = useState("");
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const OnHandleChangeItem = text => {
+  const onHandleChangeItem = text => {
     setTextItem(text)
   }
 
   const addItem = () => {
-    setList(prevState => [...prevState, textItem]);
-    setTextItem("");
+    setList(prevState => [...prevState, textItem])
+    setTextItem("")
   }
 
-  const handleModal = (item) => {
+  const handleModal = item => {
     setItemSelected(item)
     setModalVisible(true)
   }
 
-  const onHandleDelete = (item) => {
+  const onHandleDelete = item => {
     setList(prevState => prevState.filter(element => element !== item))
     setModalVisible(!modalVisible)
   }
+
+
 
   const renderItem = ({ item }) => (
     <View style={styles.renderItemStyle}>
       <Text>{item}</Text>
       <Button title="Edit" onPress={() => handleModal(item)} />
+      <Image style={styles.checkImg} source={require("./assets/bookmark-check-fill.png")} />
+      
     </View>
   )
 
-  
-
   return (
     <View style={styles.container}>
-      <View styles={styles.titleContainer}>
-        <Text style={styles.title}>Shopping List</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Tasks List</Text>
+        <AddItem
+          onChange={onHandleChangeItem}
+          textValue={textItem}
+          onAddItem={addItem}
+        />
       </View>
-
-      <View style={styles.inputContainer}>
-        <TextInput placeholder="Type your product" style={styles.addItemInput} onChangeText={OnHandleChangeItem} value={textItem} />
-        <Button title="ADD" onPress={addItem} />
-      </View>
-
       <View style={styles.listContainer}>
-        {/*list.map(item => (
-          <View>
-            <Text>{item}</Text>
-          </View>
-        ))*/}
-
-        <FlatList data={list} keyExtractor={item => item.id} renderItem={renderItem} ></FlatList>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={item => item}
+        />
       </View>
-
-      <Modal styles={styles.deleteButton} animationType="fade" transparent={true} visible={modalVisible}>
-        <View style={styles.modalContainer}>
-          <View style={styles.modalStyle}>
-            <Text style={styles.modalTextStyle}>
-              {itemSelected}
-            </Text>
-            <Button title="Delete" onPress={() => console.log("borrar elemento")} />
-            <Button title="Dismiss" onPress={() => setModalVisible(false)} />
-          </View>
-        </View>
-      </Modal>
-
+      <Modal
+        isVisible={modalVisible}
+        itemSelected={itemSelected}
+        actionDeleteItem={() => onHandleDelete(itemSelected)}
+        onDismissModal={setModalVisible}
+            
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
-    paddingTop: 80
+    backgroundColor: "#ABDBE3",
   },
   titleContainer: {
-    flex: 1,
-    padding: 50,
-    paddintTop: 80,
-  },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  addItemInput: {
-    borderBottomColor: "gray",
-    borderBottomWidth: 2,
-    width: 200
+    height: 200,
+    paddingHorizontal: 30,
+    paddingTop: 80,
   },
   title: {
     marginBottom: 30,
-    color: "gray",
-    fontSize: 20
+    fontSize: 25,
+    fontWeight: "500",
+    color: "#154C79",
+  },
+  checkImg:{
+    width: 35,
+    height: 35,
   },
   listContainer: {
     flex: 2,
-
+    marginHorizontal: 30,
+    marginTop: 40,
+    padding: 3,
   },
-  renderItemStyle:{
-    backgroundColor: "silver",
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: {width: 0, height: 2,},
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    marginTop:20,
+  renderItemStyle: {
+    height: 60,
     flexDirection: "row",
-    alignItems:"center",
-    justifyContent:"space-around",
-    elevation: 5,
-  },
-  modalContainer: {
-    flex: 1,
+    marginBottom: 25,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 10,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22,
+    shadowColor: "black",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 2,
+    elevation: 3,
   },
-  modalStyle: {
-    
-    margin: 20,
-    backgroundColor: "silver",
-    borderRadius: 10,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {width: 0, height: 2,},
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  deleteButton: {
-    paddingTop: 50,
-  },
-  modalTextStyle:{
-    fontSize: 20,
-  },
-});
+})
